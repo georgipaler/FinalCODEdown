fc09dac77d0fafa6288b8d0f13ef9119692ec29b
otiliapetcu <otilialpetcu@gmail.com>
2018-11-12 16:15:10 +0200

    update activitati

--- app/src/main/java/com/example/android/myapplication/StartActivity.java
+++ app/src/main/java/com/example/android/myapplication/StartActivity.java
@@ -1,15 +1,13 @@
 package com.example.android.myapplication;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_adevarat_fals);
     }
 }
