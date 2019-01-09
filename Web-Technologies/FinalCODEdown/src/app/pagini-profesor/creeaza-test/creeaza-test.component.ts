0311ce0e1a35ecca88b1dadcc9c56bb49e951267
Georgiana Paler <georgiana.paler@ibm.com>
2019-01-09 08:37:00 +0200

    mock up some dates and chestii pe la testele mele

--- Web-Technologies/FinalCODEdown/src/app/pagini-profesor/creeaza-test/creeaza-test.component.ts
+++ Web-Technologies/FinalCODEdown/src/app/pagini-profesor/creeaza-test/creeaza-test.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IIntrebare } from 'src/app/models';
 @Component({
   selector: 'app-creeaza-test',
   templateUrl: './creeaza-test.component.html',
   styleUrls: ['./creeaza-test.component.css']
 })
 export class CreeazaTestComponent implements OnInit {
+
+
+  public questionsList: IIntrebare[] = [];
 
   titleForm: FormGroup;
   constructor(private fb: FormBuilder) { }
@@ -22,6 +26,21 @@ export class CreeazaTestComponent implements OnInit {
 
 assignName(){
   console.log("Form title!")
+}
+
+createQuestion(type: string){
+  let q = {
+    id: 20,
+    titlu : "",
+    tip: type,
+    varianteRaspuns :[]
+  }
+  this.questionsList.push(q);
+}
+
+deleteQuestion(intrebare: IIntrebare){
+  const index = this.questionsList.findIndex(q => q.id === intrebare.id);
+  this.questionsList.splice(index, 1);
 }
 
 }
