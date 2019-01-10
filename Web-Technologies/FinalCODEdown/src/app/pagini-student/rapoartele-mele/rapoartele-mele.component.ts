b1a1a10fa21ac1204e6b3127b08be05328c5198c
Georgiana Paler <georgiana.paler@ibm.com>
2019-01-10 22:55:53 +0200

    comp

---
+++ Web-Technologies/FinalCODEdown/src/app/pagini-student/rapoartele-mele/rapoartele-mele.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rapoartele-mele',
+  templateUrl: './rapoartele-mele.component.html',
+  styleUrls: ['./rapoartele-mele.component.css']
+})
+export class RapoarteleMeleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
