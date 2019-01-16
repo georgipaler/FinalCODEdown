9db276dfbb951e606769eaa773ea637f9e420f2a
Georgiana Paler <georgiana.paler@ibm.com>
2019-01-16 12:45:14 +0200

    post test

--- Web-Technologies/workspace/client/src/app/pagini-profesor/creeaza-test/creeaza-test.component.ts
+++ Web-Technologies/workspace/client/src/app/pagini-profesor/creeaza-test/creeaza-test.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IIntrebare, ITest, TESTE } from 'src/app/models';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-creeaza-test',
   templateUrl: './creeaza-test.component.html',
@@ -15,6 +17,7 @@ export class CreeazaTestComponent implements OnInit {
   public listaTeste: Array<ITest>;
 
   testeSubscription: Subscription;
+  public testListSubject:BehaviorSubject<any> = new BehaviorSubject([]) ;
 
   titleForm: FormGroup;
   constructor(private fb: FormBuilder,
@@ -34,18 +37,21 @@ export class CreeazaTestComponent implements OnInit {
     this.titleForm = this.fb.group({
       testName: ["", [Validators.required]],
       time: ["", [Validators.required]],
-      materie: ["", [Validators.required]]
+      materie: ["", [Validators.required]],
+      codTest: ["", [Validators.required]]
     });
 }
 
 assignName(){
-  console.log("Form title!", this.titleForm.value);
+
+  this.addTest(this.newTestValue());
+  console.log("Form title post!", this.newTestValue());
 }
 
 createQuestion(type: string){
   let q = {
     id: 20+this.questionsList.length,
-    idTest: 1,
+    idTest: this.listaTeste.length,
     enunt : "",
     tipIntrebare: type
   }
@@ -56,6 +62,32 @@ removeQuestion(intrebare: IIntrebare){
   const index = this.questionsList.findIndex(q => q.id === intrebare.id);
   this.questionsList.splice(index, 1);
 }
+
+  //post tests http req 
+  addNewTestObs(test: any): Observable<any>{
+    return this.http.post("https://final-codedown-georgipaler.c9users.io/post/teste", test);
+  }
+
+  addTest(raport: any): void {
+    this.addNewTestObs(raport).subscribe(data => {
+      this.listaTeste.push(_.cloneDeep(raport));
+      this.testListSubject.next(this.listaTeste);
+
+      console.log("post", this.listaTeste)
+    });
+  }
+
+  newTestValue() {
+    let test = {
+      numeTest: this.titleForm.value.testName,
+      timp: this.titleForm.value.time,
+      materie: this.titleForm.value.materie,
+      codTest: this.titleForm.value.codTest,
+      activ: false,
+      userId: 2
+    }
+    return test;
+  }
 
 ngOnDestry(){
   this.testeSubscription.unsubscribe();
